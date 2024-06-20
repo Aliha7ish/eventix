f57ca33863990ddffcf1af7b1171ef79557bbbbf
Omar Swailam <omarswailam@outlook.com>
2024-06-20 22:34:37 +0300

    feat: fetch events

--- src/components/EventItem.jsx
+++ src/components/EventItem.jsx
@@ -12,17 +12,17 @@ function EventItem({ event, addButton = false }) {
   return (
     <Link to={`/events/event/${event.id}`}>
       <li className={styles.item}>
-        {event?.isVirtual ? <VirtualBadge /> : null}
+        {event.type === "virtual" ? <VirtualBadge /> : null}
         <div className={styles.imgContainer}>
           <img src={event.image}></img>
         </div>
         <div className={styles.eventOrgContainer}>
-          <p className={styles.eventName}>{event.eventName}</p>
-          <p className={styles.organizer}>{event.organizer}</p>
+          <p className={styles.eventName}>{event.title}</p>
+          <p className={styles.organizer}>{event.owner.name}</p>
         </div>
         <div className={styles.eventDateContainer}>
-          <p>{event.date}</p>
-          <p>{event.time}</p>
+          <p>start time: {event.start_time}</p>
+          <p>end time: {event.end_time}</p>
         </div>
         {addButton ? <ButtonComp /> : ""}
       </li>
--- src/components/EventsCollection.jsx
+++ src/components/EventsCollection.jsx
@@ -2,28 +2,56 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
+import React, { useState, useEffect } from 'react';
 import EventItem from "./EventItem";
 import styles from "../modules/EventsCollection.module.css";
 
-function EventsCollection({ collection, collectionName }) {
+function EventsCollection({ collectionName }) {
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   const fetchEvents = async function () {
-    const response = await fetch("http://127.0.0.1:8000/api/events", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await response.json();
-    return data;
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/events", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      setEvents(data.results); 
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
   };
-  console.log(fetchEvents());
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error fetching events: {error.message}</p>;
+  }
+
   return (
     <article className={`container-fluid ${styles.container}`}>
       <p className={styles.collectionName}>{collectionName}</p>
       <ul>
-        {/* {fetchEvents.map((event) => {
+        {events.map((event) => {
           return <EventItem key={event.id} event={event} />;
-        })} */}
+        })}
       </ul>
     </article>
   );
